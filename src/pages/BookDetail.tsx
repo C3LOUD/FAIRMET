08bf038fde67a253ffc98085a820465f67246c66
C3LOUD <tommy51105110@gmail.com>
2023-11-30 00:11:39 +0800

    feat: update book detail page ui

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -1,7 +1,10 @@
 import {
   Box,
-  Button,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
   Container,
+  Divider,
   Flex,
   HStack,
   Heading,
@@ -9,14 +12,16 @@ import {
   Skeleton,
   Spacer,
   Text,
-  VStack,
 } from "@chakra-ui/react";
 import { faker } from "@faker-js/faker";
 import { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { BookCardLayout } from "../components/BookCard";
+import BookCarousel from "../components/BookCarousel";
+import BrandCarousel from "../components/BrandCarousel";
+import BrandHeader from "../components/BrandHeader";
 import { TBook, TagKey } from "../types";
 import { getBook } from "../util/getBook";
-import { FaArrowLeft } from "react-icons/fa6";
 
 const bookTags: TagKey[] = [
   "Style, Occasion & Dressing Type",
@@ -33,7 +38,6 @@ const BookDetail = () => {
   const [previousBook, setPreviousBook] = useState<TBook>();
   const [nextBook, setNextBook] = useState<TBook>();
   const { id } = useParams();
-  const navigate = useNavigate();
 
   useEffect(() => {
     if (!id) return;
@@ -45,51 +49,124 @@ const BookDetail = () => {
   }, [id]);
   return (
     <Container px="0" maxW="100%" w="100%" mb="8rem">
-      <VStack borderBottom="solid 1px" mx="3rem" position="relative">
-        <Button
-          position="absolute"
-          bottom="0"
-          left="0"
-          transform="auto"
-          translateY="150%"
-          variant="link"
-          onClick={() => {
-            navigate("/book");
-          }}
-        >
-          <FaArrowLeft />
-        </Button>
-        <Heading>{"Post"}</Heading>
-        <Text>
-          {"Seek your Air, Build your Wardrobe \u0026 Find your Lifestyle"}
-        </Text>
-      </VStack>
-      <Box h="3rem" />
       <Skeleton isLoaded={Boolean(book)} px="3rem">
         {Boolean(book) && (
           <>
-            <Text fontWeight={700}>{"このページに関連するタグ"}</Text>
-            <Box h="1rem" />
-            <Flex gap="0.5rem" flexWrap="wrap">
-              {Object.keys(book!.tags)
-                .reduce<string[]>(
-                  (acc, el) => [...acc, ...book!.tags[el as TagKey]],
-                  [] as string[]
-                )
-                .map((tag: string, i) => (
-                  <Box
-                    fontStyle="italic"
-                    fontWeight={700}
-                    textColor="shade.500"
-                    border="solid 1px"
-                    px="0.5rem"
-                    fontSize={14}
-                    key={i}
-                  >
-                    {tag}
-                  </Box>
-                ))}
-            </Flex>
+            <Breadcrumb borderBottom="solid 1px" w="fit-content">
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/">Home</BreadcrumbLink>
+              </BreadcrumbItem>
+
+              <BreadcrumbItem>
+                <BreadcrumbLink href={`/book#${book?.category}`}>
+                  {book?.category}
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+
+              <BreadcrumbItem isCurrentPage>
+                <BreadcrumbLink href="#">{book?.title}</BreadcrumbLink>
+              </BreadcrumbItem>
+            </Breadcrumb>
+            <Box h="3rem" />
+
+            <Box position="relative">
+              <Box w="95%" overflow="hidden" mx="auto">
+                <HStack h="18rem" w="max-content">
+                  {Array.from({ length: 5 }, (_, index) => index + 1).map(
+                    (index) => (
+                      <Box h="100%" key={index}>
+                        <Image
+                          src={faker.image.url()}
+                          objectFit="cover"
+                          w="100%"
+                          h="100%"
+                          objectPosition="center"
+                        />
+                      </Box>
+                    )
+                  )}
+                </HStack>
+              </Box>
+              <Box
+                position="absolute"
+                w="70%"
+                p="1rem"
+                h="fit-content"
+                maxH="16rem"
+                bgColor="white"
+                left="50%"
+                transform="auto"
+                translateX="-50%"
+                bottom="0"
+                translateY="70%"
+              >
+                <Heading
+                  w="100%"
+                  overflow="hidden"
+                  css={{
+                    display: "-webkit-box",
+                    WebkitBoxOrient: "vertical",
+                    WebkitLineClamp: 2,
+                  }}
+                >
+                  {book?.title}
+                </Heading>
+                <Box h="2rem" />
+                <Text>
+                  {"Seek your air, build your wardrobe & find your lifestyle"}
+                </Text>
+                <Box h="1rem" />
+                <Text
+                  w="100%"
+                  overflow="hidden"
+                  css={{
+                    display: "-webkit-box",
+                    WebkitBoxOrient: "vertical",
+                    WebkitLineClamp: 3,
+                  }}
+                >
+                  {book?.content}
+                </Text>
+              </Box>
+            </Box>
+
+            <Box h="14rem" />
+            <HStack justifyContent="end">
+              <HStack
+                borderBottom="solid 2px"
+                borderColor="shade.500"
+                w="fit-content"
+                pl="2rem"
+                pb="0.5rem"
+                fontWeight={700}
+                textColor="shade.500"
+                gap="1rem"
+              >
+                <Text>{`\u3010${book?.category}\u3011`}</Text>
+                <Divider
+                  orientation="vertical"
+                  h="1rem"
+                  borderColor="secondary"
+                />
+                <Text>
+                  {new Intl.DateTimeFormat("zh-TW", {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                  })
+                    .format(faker.date.anytime())
+                    .split("/")
+                    .join(".")}
+                </Text>
+                <Divider
+                  orientation="vertical"
+                  h="1rem"
+                  borderColor="secondary"
+                />
+                <Text>{`By: ${faker.person.fullName()}`}</Text>
+              </HStack>
+            </HStack>
+
             <Box h="3rem" />
             <Flex h="fit-content" gap="2rem">
               <Flex flex="1" direction="column" gap="1rem" minH="30rem">
@@ -160,7 +237,34 @@ const BookDetail = () => {
                 </Flex>
               </Flex>
             </Flex>
+            <Box h="3rem" />
+            <Text fontWeight={700}>
+              {"Tags / このページに関連するタグ \u3010The Relevant\u3011"}
+            </Text>
+            <Box h="1rem" />
+            <Flex gap="0.5rem" flexWrap="wrap">
+              {Object.keys(book!.tags)
+                .reduce<string[]>(
+                  (acc, el) => [...acc, ...book!.tags[el as TagKey]],
+                  [] as string[]
+                )
+                .map((tag: string, i) => (
+                  <Box
+                    fontStyle="italic"
+                    fontWeight={700}
+                    textColor="shade.500"
+                    border="solid 1px"
+                    px="0.5rem"
+                    fontSize={14}
+                    key={i}
+                  >
+                    {tag}
+                  </Box>
+                ))}
+            </Flex>
             <Box h="4rem" />
+            <Divider borderColor="secondary" />
+            <Box h="2rem" />
             <HStack>
               <Box
                 maxW="30%"
@@ -171,7 +275,8 @@ const BookDetail = () => {
                 }}
               >
                 <Link to={`/book/${previousBook?.id}`}>
-                  {`previous: ${previousBook?.title}`}
+                  <Text>{"Previous:"}</Text>
+                  {previousBook?.title}
                 </Link>
               </Box>
               <Spacer />
@@ -184,13 +289,29 @@ const BookDetail = () => {
                 }}
               >
                 <Link to={`/book/${nextBook?.id}`}>
-                  {`next: ${nextBook?.title}`}
+                  <Text>{"Next:"}</Text>
+                  {nextBook?.title}
                 </Link>
               </Box>
             </HStack>
+            <Box h="2rem" />
+            <Divider borderColor="secondary" />
           </>
         )}
       </Skeleton>
+      <Box h="5rem" />
+      <Box bgColor="primary" py="1rem">
+        <Text px="1rem" fontWeight={700} fontSize={20}>
+          {"Recommend / Relevant Post"}
+        </Text>
+        <BookCarousel type={BookCardLayout.Portrait} />
+      </Box>
+      <Box h="5rem" />
+      <BrandHeader />
+      <Box h="3rem" />
+      <BrandCarousel />
+      <Box h="3rem" />
+      <Divider borderColor="secondary" />
     </Container>
   );
 };
